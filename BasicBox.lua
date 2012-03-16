b7d2f1a6cab5302c74019042c464d1480df186c1
jdpasqua <justin.pasqualini@gmail.com>
2012-03-16 13:51:04 -0400

    Updated BasicBox to have new graphics and some new movement ideas.

--- BasicBox.lua
+++ BasicBox.lua
@@ -1,15 +1,22 @@
 module (..., package.seeall)
 
-function new()
+function new(type)
 	
 	local Bullet = require("Bullet")
+	local easingx  = require("easing")
 	
-	local box = display.newRect( 0, 0, 30, 30 )
+	local box
+	if (math.random(10) > 7) then 
+		box = display.newImage("assets/graphics/enemy5.png")
+	else 
+		box = display.newImage("assets/graphics/enemy12.png")
+	end
 	
+	box:scale(2, 2)
 	local straightBullet
 	
 	local transitionManager = require('transitionManager')
-	
+
 	function box:init()
 
 		-- Physics
@@ -18,12 +25,14 @@ function new()
 		-- Name
 		box.name = "enemy"
 		
-		-- Position
-		box.x = math.random(display.contentWidth)
-		box.y = math.random(display.contentHeight / 2)
+		box:setReferencePoint( display.CenterReferencePoint )
 		
+		-- Position
+		box.x = math.floor(math.random(display.contentHeight / 2))
+		box.y = math.floor(math.random(display.contentHeight / 2))
+				
 		-- Color
-		box.setColor()
+		--box.setColor()
 		
 		-- Event Listener
 		Runtime:addEventListener( "pulse", box )
@@ -41,9 +50,30 @@ function new()
 		else
 			rotationAngle = 0
 		end
-		local trans1 = transitionManager:newTransition( box, { rotation = rotationAngle, 
-			time=900, x=math.random(display.contentWidth), y=math.random(display.contentHeight / 2)} )
-	
+		--local trans1 = transitionManager:newTransition( box, { --rotation = 0, 
+		--	time=900, x=math.random(display.contentWidth), y=math.random(display.contentHeight / 2)} )
+		--[[
+		local nextX
+		local nextY
+		
+		if (box.x > 350) then
+			nextX = box.x - 200
+		else
+			nextX = box.x + 200
+		end
+		
+		if (box.y > 500) then
+			nextY = box.y - 500
+		else
+			nextY = box.y + 100
+		end
+		
+		transition.to(box, {
+		                time = 4000,
+		                x = display.contentWidth + 10 ,
+		                y = display.contentHeight / 2 - box.y,
+		                transition = easingx.easeInElastic })
+						]]
 	end
 	
 	function box:setColor()
@@ -52,7 +82,7 @@ function new()
 	
 	function box:shoot()
 		-- Fire a bullet!
-		straightBullet = Bullet.new(box.x, box.y)
+		straightBullet = Bullet.new(box.x, box.y + math.floor(box.height / 2))
 	end
 	
 	function box:pulse( event )
