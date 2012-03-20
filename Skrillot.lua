f97a03edeb13c7f671873e0c38f8d416b04620d0
jdpasqua <justin.pasqualini@gmail.com>
2012-03-19 20:47:55 -0400

    Enhanced the Skrillot enemy.

--- Skrillot.lua
+++ Skrillot.lua
@@ -5,18 +5,17 @@ function new(type)
 	local BulletRotating = require("BulletRotating")
 	local easingx  = require("easing")
 	
-
 	local box = display.newImage("assets/graphics/Skrillot.png")
 	box:scale(2, 2)
 	
 	local straightBullet
-	local rotationAngle = 90
-	
+	local rotationAngle = 135
+	local isShooting = false 
 	local transitionManager = require('transitionManager')
-
-	--function box:onCollision(self, event)
-	--	print ("COLLISION: " .. self.name .. " WITH " .. event.other.name)
-	--end 
+	
+	function box:enableShooting() 
+		isShooting = true
+	end
 	
 	function box:init()
 
@@ -31,52 +30,24 @@ function new(type)
 		box:setReferencePoint( display.CenterReferencePoint )
 		
 		-- Position
-		box.x = math.floor(math.random(display.contentHeight / 2))
-		box.y = math.floor(math.random(display.contentHeight / 2))
-				
-		-- Color
-		--box.setColor()
+		box.x = math.floor(math.random(display.contentWidth))
+		box.y = math.floor(math.random(display.contentHeight / 2) - display.contentHeight + 30)
 		
+		transition.to(box, {
+		                time = 4000,
+		                x = math.floor(math.random(display.contentWidth - 80) + 40),
+		                y = math.floor(math.random(display.contentHeight / 2)),
+		                transition = easingx.easeOutBack,
+		 				onComplete = box.enableShooting })
+						
 		-- Event Listener
 		Runtime:addEventListener( "pulse", box )
-		
-		--box.collision = onCollision
-		--box:addEventListener("collision", box)
-				
+
 		_G.gameLayer:insert(box)
 	end
 	
 	function box:move()
-		--local rotationAngle
-		--if (box.rotation == 0) then
-	--		rotationAngle = 360
---		else
---			rotationAngle = 0
---		end
-		--local trans1 = transitionManager:newTransition( box, { --rotation = 0, 
-		--	time=900, x=math.random(display.contentWidth), y=math.random(display.contentHeight / 2)} )
-		--[[
-		local nextX
-		local nextY
-		
-		if (box.x > 350) then
-			nextX = box.x - 200
-		else
-			nextX = box.x + 200
-		end
-		
-		if (box.y > 500) then
-			nextY = box.y - 500
-		else
-			nextY = box.y + 100
-		end
-		
-		transition.to(box, {
-		                time = 4000,
-		                x = display.contentWidth + 10 ,
-		                y = display.contentHeight / 2 - box.y,
-		                transition = easingx.easeInElastic })
-						]]
+
 	end
 	
 	function box:setColor()
@@ -85,10 +56,10 @@ function new(type)
 	
 	function box:shoot()
 		-- Fire a bullet!
-		straightBullet = BulletRotating.new(box.x, box.y + math.floor(box.height / 2), rotationAngle)
-		rotationAngle = rotationAngle + 15
-		--print ("ROTATION ANGLE" .. rotationAngle)
-		
+		if (isShooting) then
+			straightBullet = BulletRotating.new(box.x, box.y + math.floor(box.height / 2), rotationAngle)
+			rotationAngle = rotationAngle + 10
+		end
 	end
 	
 	function box:pulse( event )
