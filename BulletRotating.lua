9303ad75fbd42c31e4d58a93b4f102e8f62fd96e
jdpasqua <justin.pasqualini@gmail.com>
2012-03-21 14:08:28 -0400

    Refactored the BulletRotating class to inherit from the BasicBullet class. It's much cleaner now!

--- BulletRotating.lua
+++ BulletRotating.lua
@@ -2,31 +2,12 @@ module (..., package.seeall)
 
 function new(xPos, yPos, rotationAngle)
 	
-	if ((xPos == nil) or (yPos == nil)) then
-		return nil
-	end
-	
-	local bullet = display.newCircle(xPos, yPos, 6)
-	--display.newImage("assets/graphics/bullet10.png", xPos, yPos + 40)
+	local BasicBullet = require("BasicBullet")
 
-	function bullet:removeBullet()
-		bullet:removeSelf()
-	end
+	local bullet = BasicBullet.new(xPos, yPos, "enemyBullet", "assets/graphics/bullet10.png", true)
 	
 	function bullet:init()
 		
-		local enemyBulletCollisionFilter = { categoryBits = 8, maskBits = 1 }
-		
-		-- Physics
-		physics.addBody(bullet, "dynamic", {bounce = 0, filter = enemyBulletCollisionFilter})
-		
-		-- Name
-		bullet.name = "enemyBullet"
-		
-		bullet:setReferencePoint( display.CenterReferencePoint )
-		
-		-- Color
-		bullet.setColor()
 		bullet.rotation = rotationAngle
 		bullet.isFixedRotation = true
 	
@@ -34,16 +15,6 @@ function new(xPos, yPos, rotationAngle)
 		bullet:setLinearVelocity(math.sin(math.rad(bullet.rotation))*bulletSpeed,
 								 math.cos(math.rad(bullet.rotation))*-bulletSpeed)
 		timer.performWithDelay(10000, bullet.removeBullet)
-
-		_G.bulletsLayer:insert(bullet)
-	end
-	
-	function bullet:move()
-		--transition.to( bullet, { time=900, x=math.random(display.contentWidth), y=math.random(display.contentHeight / 2)} )
-	end
-	
-	function bullet:setColor()
-		bullet:setFillColor(131, 245, 44)
 	end
 	
 	bullet:init()	
