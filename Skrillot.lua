441e19e7107053b5dbe9a57a0e3befc3e3ef08a3
James Swidersky <citystars19@hotmail.com>
2012-03-26 19:51:54 -0400

    Enemy swarms implemented.

--- Skrillot.lua
+++ Skrillot.lua
@@ -10,7 +10,7 @@ function new(type, entrance)
 	local trackno = "track7"
 	
 	local BasicEnemy = require("BasicEnemy")
-	local enemy = BasicEnemy.new("Skrillot", "assets/graphics/Skrillot.png", trackno, 10)
+	local enemy = BasicEnemy.new("Skrillot", "assets/graphics/Skrillot.png", trackno, 3)
 	
 	local bullet1
 	local bullet2
