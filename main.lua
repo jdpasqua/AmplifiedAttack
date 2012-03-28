48bda4ded9dcc00843001ac93494bf45d7c6433a
jdpasqua <justin.pasqualini@gmail.com>
2012-03-28 18:04:59 -0400

    Reverted main.lua

--- main.lua
+++ main.lua
@@ -49,7 +49,7 @@ local main = function ()
 	-- Change scene without effects
 	------------------
 	
-	director:changeScene("levelSelector")
+	director:changeScene("level1")
 	
 	------------------
 	-- Return
